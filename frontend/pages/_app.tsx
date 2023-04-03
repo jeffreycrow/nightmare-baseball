07bcecd77dda020862df6b5b7ee89040646f75de
Jeffrey Crow <jeff@jeffreycrow.com>
2023-04-03 15:53:40 -0500

    initial commit

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '@/styles/globals.css'
+
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps<{
+  initialSession: Session, }>) {
+
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+}
